38b8b5696aa07eb8dce566ba8063a828282f7745
Mikhail Korenkov <mikhail.korenkov@dualbootpartners.com>
2022-07-14 14:12:57 +0300

    finished fp/map-reduce

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,41 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(films)
+        suitable_films_number = 0
+        accumulated_film_rating = films.reduce(0.0) { |sum, film|
+          current_rating_kinopoisk = film["rating_kinopoisk"] && film["rating_kinopoisk"].to_f
+          number_of_countries = film["country"] && film["country"].split(",").length || 0
+          if (current_rating_kinopoisk && current_rating_kinopoisk != 0 && number_of_countries >= 2)
+            suitable_films_number += 1
+            sum + film["rating_kinopoisk"].to_f
+          else
+            sum
+          end
+        }
+        return accumulated_film_rating / suitable_films_number
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        letter_to_count = "и"
+        number_of_letters_per_each = films.map { |film|
+          rating_kinopoisk = film["rating_kinopoisk"] && film["rating_kinopoisk"].to_f
+          if (rating_kinopoisk && rating_kinopoisk >= threshold)
+            number_of_letters_to_count = film["name"].split("").reduce(0) { |acc, symbol|
+              if (symbol == letter_to_count)
+                acc + 1
+              else
+                acc
+              end
+            }
+            number_of_letters_to_count
+          else
+            0
+          end
+        }
+        return number_of_letters_per_each.reduce(0) { |acc, number|
+                 acc + number
+               }
       end
     end
   end
