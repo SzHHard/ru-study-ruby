f0f6d179b8ca1c824d7819c9e875bbb5a652e4b7
Mikhail Korenkov <mikhail.korenkov@dualbootpartners.com>
2022-07-14 14:19:28 +0300

    ran lint

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -6,39 +6,39 @@ module Exercise
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(films)
         suitable_films_number = 0
-        accumulated_film_rating = films.reduce(0.0) { |sum, film|
-          current_rating_kinopoisk = film["rating_kinopoisk"] && film["rating_kinopoisk"].to_f
-          number_of_countries = film["country"] && film["country"].split(",").length || 0
-          if (current_rating_kinopoisk && current_rating_kinopoisk != 0 && number_of_countries >= 2)
+        accumulated_film_rating = films.reduce(0.0) do |sum, film|
+          current_rating_kinopoisk = film['rating_kinopoisk']&.to_f
+          number_of_countries = film['country']&.split(',')&.length || 0
+          if current_rating_kinopoisk && current_rating_kinopoisk != 0 && number_of_countries >= 2
             suitable_films_number += 1
-            sum + film["rating_kinopoisk"].to_f
+            sum + film['rating_kinopoisk'].to_f
           else
             sum
           end
-        }
-        return accumulated_film_rating / suitable_films_number
+        end
+        accumulated_film_rating / suitable_films_number
       end
 
       def chars_count(films, threshold)
-        letter_to_count = "и"
-        number_of_letters_per_each = films.map { |film|
-          rating_kinopoisk = film["rating_kinopoisk"] && film["rating_kinopoisk"].to_f
-          if (rating_kinopoisk && rating_kinopoisk >= threshold)
-            number_of_letters_to_count = film["name"].split("").reduce(0) { |acc, symbol|
-              if (symbol == letter_to_count)
+        letter_to_count = 'и'
+        number_of_letters_per_each = films.map do |film|
+          rating_kinopoisk = film['rating_kinopoisk']&.to_f
+          if rating_kinopoisk && rating_kinopoisk >= threshold
+            number_of_letters_to_count = film['name'].chars.reduce(0) do |acc, symbol|
+              if symbol == letter_to_count
                 acc + 1
               else
                 acc
               end
-            }
+            end
             number_of_letters_to_count
           else
             0
           end
-        }
-        return number_of_letters_per_each.reduce(0) { |acc, number|
-                 acc + number
-               }
+        end
+        number_of_letters_per_each.reduce(0) do |acc, number|
+          acc + number
+        end
       end
     end
   end
--- test/exercise/fp/test.rb
+++ test/exercise/fp/test.rb
@@ -1,13 +1,13 @@
-require "csv"
-require "./test/test_helper"
-require_relative "./solution"
+require 'csv'
+require './test/test_helper'
+require_relative './solution'
 
 # Оба задания нужно решить используя map и reduce
 class Exercise::FpTest < Minitest::Test
   # Посчитать средний рейтинг фильмов по версии кинопоиска у которых две или больше стран
   # Фильмы у которых рейтиг не задан или равен 0 не учитывать в расчете среднего.
   def test_rating
-    array = CSV.readlines("./test/fixtures/films.csv", headers: true)
+    array = CSV.readlines('./test/fixtures/films.csv', headers: true)
 
     result = Exercise::Fp.rating(array)
     # rubocop:disable Lint/FloatComparison
@@ -17,7 +17,7 @@ class Exercise::FpTest < Minitest::Test
 
   # Посчитать количесвто букв 'и' в названиях всех фильмов с рейтингом кинопоиска больше или равным заданному значению
   def test_chars_count
-    array = CSV.readlines("./test/fixtures/films.csv", headers: true)
+    array = CSV.readlines('./test/fixtures/films.csv', headers: true)
 
     result = Exercise::Fp.chars_count(array, 5)
     assert result == 3850
