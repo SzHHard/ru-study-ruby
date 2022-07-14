cbfab41ea09a2b48c87465b45132a7a5d7b5ce58
Mikhail Korenkov <mikhail.korenkov@dualbootpartners.com>
2022-07-14 18:07:49 +0300

    finished fp2 task, ran lint

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,40 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each
+        for i in self
+          yield i
+        end
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        modified_array = self.class.new(self)
+        for i in (0...length)
+          modified_array[i] = yield self[i]
+        end
+        modified_array
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        new_arr = self.class.new([])
+        my_each { |el| !el.nil? && new_arr.push(el) }
+        new_arr
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(initial = nil)
+        acc = initial
+        for i in self
+          if acc.nil?
+            acc = i
+            next
+          end
+          acc = yield(acc, i)
+        end
+        acc
+      end
     end
   end
 end
